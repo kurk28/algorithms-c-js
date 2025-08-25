9167d919809d3ce153239e5294b4f06d95e949c5
Ilia Dubovik <askdubovik@gmail.com>
2025-08-25 14:46:06 +0200

    js: add weighted quick-union algorithm with path compression

---
+++ ch01/01-08.js
@@ -0,0 +1,42 @@
+// weighted quick-union algorithm with path compression
+
+let mockInput = [
+  [0, 2],
+  [1, 4],
+  [2, 5],
+  [3, 6],
+  [0, 4],
+  [6, 0],
+  [1, 3],
+];
+
+function weightedQuickUnionWithPC(size, input) {
+  const id = new Array(size);
+  const sz = new Array(size);
+  for (let i = 0; i < size; i++) {
+    id[i] = i;
+    sz[i] = 1;
+  }
+  let i, j;
+  for (let pair of input) {
+    for (i = pair[0]; i != id[i]; i = id[i]) {
+      id[i] = id[id[i]];
+    }
+    for (j = pair[1]; j != id[j]; j = id[j]) {
+      id[j] = id[id[j]];
+    }
+    if (i === j) continue;
+
+    if (sz[i] < sz[j]) {
+      id[i] = j;
+      sz[j] += sz[i];
+    } else {
+      id[j] = id[i];
+      sz[i] += sz[j];
+    }
+
+    console.log(id);
+  }
+}
+
+weightedQuickUnionWithPC(10, mockInput);
